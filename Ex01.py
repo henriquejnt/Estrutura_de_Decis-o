21e3c1297d6d22d67bf17cd6386e9ec4adced630
henriquejnt <henriquent.dev@gmail.com>
2024-11-07 10:14:09 -0300

    exercicio 01

---
+++ Ex01.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+n1 = float(input("N1:"))
+n2 = float(input("N2:"))
+if n1 > n2:
+    print(f"O maior número é {n1}")
+else:
+    print(f"O maior número é {n2}")
