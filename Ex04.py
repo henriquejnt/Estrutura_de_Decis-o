09bbd0c6841e541d34338cd8bd46d21efd17a216
henriquejnt <henriquent.dev@gmail.com>
2024-11-09 11:15:02 -0300

    Vogal ou consoante

---
+++ Ex04.py
@@ -0,0 +1,14 @@
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+letra = input("Digite uma letra: ").lower()
+if letra == 'a':
+    print(f"A letra {letra} é uma vogal !")
+elif letra == 'e':
+    print(f"A letra {letra} é uma vogal !")
+elif letra == 'i':
+    print(f"A letra {letra} é uma vogal !")
+elif letra == 'o':
+    print(f"A letra {letra} é uma vogal !")
+elif letra == 'u':
+    print(f"A letra {letra} é uma vogal !")
+else:
+    print(f"A letra {letra} é uma consoante !")
